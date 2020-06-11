33d74d87b7c82ce5c070bbf9b13f14ab28bf35dd
seungkyun_park <phcmas1@gmail.com>
2020-06-11 20:07:23 +0900

    commit

--- algorithm/dynamic_program/packing.cc
+++ algorithm/dynamic_program/packing.cc
@@ -4,15 +4,13 @@
 #include <vector>
 using namespace std;
 
-int number_of_test_cases = 1;
-int number_of_items =6;
-int volume_of_carrier =10;
+int number_of_test_cases = 0;
+int number_of_items =0;
+int volume_of_carrier =0;
 int cache[1001][100];
 int volume[100];
 int need[100];
-int answers[50];
 string item_names[100];
-vector<int> choices[100];
 
 int NumberOfPacking (int remained_volume, int item) {
     if (item == number_of_items) return 0;
@@ -23,7 +21,7 @@ int NumberOfPacking (int remained_volume, int item) {
 
     ret = NumberOfPacking (remained_volume, item+1);
     if (remained_volume >= volume[item]) {
-        cand = NumberOfPacking (remained_volume-volume[item], item+1) + need[item];
+        ret = max(NumberOfPacking (remained_volume-volume[item], item+1) + need[item], ret);
     }
 
     if (cand > ret) ret = cand;
@@ -31,13 +29,13 @@ int NumberOfPacking (int remained_volume, int item) {
     return ret;
 } 
 
-void Reconstruct(int test_num, int remained_volume, int item, vector<int> &picked) {
+void Reconstruct(int remained_volume, int item, vector<int> &picked) {
     if (item == number_of_items) return;
     if (NumberOfPacking(remained_volume,item) == NumberOfPacking(remained_volume, item+1)) {
-        Reconstruct(test_num, remained_volume, item+1, picked);
+        Reconstruct(remained_volume, item+1, picked);
     } else {
         picked.push_back(item);
-        Reconstruct(test_num, remained_volume-volume[item],item+1,picked);
+        Reconstruct(remained_volume-volume[item],item+1,picked);
     }
 }
 
@@ -45,19 +43,18 @@ int main() {
     cin >> number_of_test_cases;
 
     for (int i=0; i < number_of_test_cases; ++i) {
-        memset (cache,-1, sizeof(cache));
+        vector<int> picked;
         cin >> number_of_items >> volume_of_carrier;
         for (int j=0; j< number_of_items; ++j) {
             cin >> item_names[j] >> volume[j] >> need[j];
         }
-        Reconstruct(i, volume_of_carrier, 0, choices[i]);
-    }
+        memset (cache,-1, sizeof(cache));
+        Reconstruct(volume_of_carrier, 0, picked);
 
-    for (int i=0; i < number_of_test_cases; ++i) {
-        int size = choices[i].size();
-        cout << answers[i] << " "  << size <<endl;
-        for (int j=0; j < size; ++j) {
-            cout << item_names[choices[i][j]] << endl;
+        cout << NumberOfPacking(volume_of_carrier,0) << " " <<picked.size() <<endl;
+
+        for (int j=0; j<picked.size(); ++j) {
+            cout << item_names[picked[j]] << endl;
         }
     }
 
