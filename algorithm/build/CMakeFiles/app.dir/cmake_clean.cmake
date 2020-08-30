e6bf97103fbb944b1e2c46edca48e76126f79a9a
seungkyun_park <phcmas1@gmail.com>
2020-08-31 02:31:17 +0900

    commit

--- algorithm/build/CMakeFiles/app.dir/cmake_clean.cmake
+++ algorithm/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -1,5 +1,5 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/app.dir/jongman_book/dfs/dictionary.o"
+  "CMakeFiles/app.dir/leet_code/0830.o"
   "app.pdb"
   "app"
 )
--- algorithm/jongman_book/dfs/dictionary.cc
+++ algorithm/jongman_book/dfs/dictionary.cc
@@ -7,7 +7,7 @@ using namespace std;
 
 int number_of_test_cases;
 int number_of_words;
-vector<vector<int>> adj(26);
+vector<vector<bool>> adj (26, vector<bool>(26, false));
 vector<bool> visited (26, false);
 vector<string> words;
 string temp;
@@ -17,13 +17,11 @@ void dfs (int here, string &dfsOrder) {
     bool alreadyVisit = visited[here];
     visited[here] = true;
 
-    for (int i=0; i < adj[here].size(); ++i) {
-        int next = adj[here][i];
-        
-        if (!visited[next]) dfs(next, dfsOrder);
+    for (int i=0; i < adj.size(); ++i) {
+        if (adj[here][i] && !visited[i]) dfs (i, dfsOrder);
     }
 
-    if (!alreadyVisit) dfsOrder +=  (char)(here+'a');
+    if (!alreadyVisit) dfsOrder += (char)(here + 'a');
 }
 
 void makeGraph (const vector<string> &words) {
@@ -38,7 +36,7 @@ void makeGraph (const vector<string> &words) {
 
         for (int j=0; j < len; ++j) {
             if (word1[j] != word2[j]) {
-                adj[word1[j]-'a'].push_back(word2[j]-'a');
+                adj[word1[j]-'a'][word2[j]-'a'] = true;
                 break;
             }
         }
@@ -50,37 +48,63 @@ string orderOfAlphabet (const vector<string> &words) {
 
     makeGraph(words);
 
-    for (int i=0; i < 26; ++i) {
+    for (int i=0; i < adj.size(); ++i) {
+        for (int j=0; j < adj.size(); ++j) {
+            if (adj[i][j] && adj[j][i]) return "INVALID HYPOTHESIS";
+        }
+    }
+
+    for (int i=0; i < adj.size(); ++i) {
         string dfsOrder;
         dfs (i, dfsOrder);
 
-        for (int j=dfsOrder.length()-1; j>=0; --j) {
-            answer += dfsOrder[j];
+        for (int j=0; j< dfsOrder.length(); ++j) {
+            answer = dfsOrder[j] + answer;
         }
     }
 
     return answer;
 }
 
+void initialize() {
+    adj.clear();
+    visited.clear();
+    words.clear();
+
+    for (int i=0; i<26; ++i) {
+        adj.push_back(vector<bool>(26, false));
+        visited.push_back(false);
+    }
+
+}
+
 int main() {
-    //cin >> number_of_test_cases;
+    cin >> number_of_test_cases;
 
-    number_of_test_cases = 1;
-    number_of_words = 5;
+    //number_of_test_cases = 1;
+    //number_of_words = 5;
 
-    words.push_back("gg");
-    words.push_back("kia");
-    words.push_back("lotte");
-    words.push_back("lg");
-    words.push_back("hanwha");
+    //words.push_back("gg");
+    //words.push_back("kia");
+    //words.push_back("lotte");
+    //words.push_back("lg");
+    //words.push_back("hanwha");
+
+    //words.push_back("dictionary");
+    //words.push_back("english");
+    //words.push_back("is");
+    //words.push_back("ordered");
+    //words.push_back("ordinary");
+    //words.push_back("this");
 
     for (int i=0; i < number_of_test_cases; ++i) {
-        //cin >> number_of_words;
+        cin >> number_of_words;
+        initialize();
 
-        //for (int j=0; j < number_of_words; ++j) {
-        //    cin >> temp;
-        //    words.push_back(temp);
-        //}
+        for (int j=0; j < number_of_words; ++j) {
+            cin >> temp;
+            words.push_back(temp);
+        }
 
         answer = orderOfAlphabet(words);
         cout << answer << endl;
