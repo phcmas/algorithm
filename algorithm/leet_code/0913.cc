1e9d1f8efd65036d7dd0298fb8e9d118fe01c776
seungkyun_park <phcmas1@gmail.com>
2020-09-13 22:15:46 +0900

    cm

--- algorithm/leet_code/0913.cc
+++ algorithm/leet_code/0913.cc
@@ -14,6 +14,7 @@
  *  **/
 
 #include <vector>
+#include <iostream>
 #include <math.h>
 
 using namespace std;
@@ -32,39 +33,88 @@ int isIn (int point, vector<int> &interval) {
 }
 
 vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    vector<vector<int>> answer;
+    vector<int> mergedInterval;
     int start = newInterval.front();
     int end = newInterval.back();
-    int overlapStart = -1;
-    int overlapEnd = -1;
-    int newStart;
-    int newEnd;
+    float overlapStart = intervals.size();
+    float overlapEnd = intervals.size();
+    int discriminant;
+
+    if (intervals.empty()) {
+        answer.push_back(newInterval);
+        return answer;
+    }
 
     for (int i=0; i < intervals.size(); ++i) {
-        int discriStart = isIn (start, intervals[i]);
-        if (discriStart == -1 || discriStart ==0) {
+        discriminant = isIn (start, intervals[i]);
+        if (discriminant != 1) {
             overlapStart = i;
-            newStart = min (intervals[i].front(), start);
+            start = min (start, intervals[i].front());
             break;
         }
     }
 
-    for (int i=overlapStart; i < intervals.size(); ++i) {
-        int discriEnd = isIn (end, intervals[i]);
-        if (discriEnd == 0 || discriEnd == 1) {
-            
+    for (int i=overlapStart; i< intervals.size(); ++i) {
+        discriminant = isIn(end, intervals[i]);
+        if (discriminant == 0) {
+            overlapEnd = i;
+            end = max (end, intervals[i].back());
+            break;
+        } else if (discriminant == -1) {
+            overlapEnd = i-0.5;
+            break;
         }
     }
 
+    mergedInterval.push_back(start);
+    mergedInterval.push_back(end);
 
+    for (int i=0; i < overlapStart; ++i) {
+        answer.push_back(intervals[i]);
+    }
+    answer.push_back(mergedInterval);
+
+    for (int i=overlapEnd+1; i < intervals.size(); ++i) {
+        answer.push_back(intervals[i]);
+    }
+
+    return answer;
 }
 
 int main() {
+    vector<vector<int>> intervals;
+    vector<int> newInt;
+    vector<int> int1;
+    vector<int> int2;
+    vector<int> int3;
+    vector<int> int4;
+    vector<int> int5;
+    int1.push_back(1);
+    int1.push_back(5);
+    //int2.push_back(3);
+    //int2.push_back(5);
+    //int3.push_back(6);
+    //int3.push_back(7);
+    //int4.push_back(8);
+    //int4.push_back(10);
+    //int5.push_back(12);
+    //int5.push_back(16);
+    intervals.push_back(int1);
+    //intervals.push_back(int2);
+    //intervals.push_back(int3);
+    //intervals.push_back(int4);
+    //intervals.push_back(int5);
+    newInt.push_back(6);
+    newInt.push_back(8);    
+
+    vector<vector<int>> answer = insert (intervals, newInt);
+
+    for (int i=0; i < answer.size(); ++i) {
+        cout << answer[i].front() << "," << answer[i].back() <<endl;
+    }
 
     return 0;
 }
-
-
-
-
 
 
