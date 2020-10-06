6ed54ab8119cc8e3319492e600c21fa6f8626428
seungkyun_park <phcmas1@gmail.com>
2020-10-07 01:02:59 +0900

    ignore

--- cpp_code/node.cc
+++
@@ -1,8 +0,0 @@
-#include "node.h"
-
-Node::Node (int data): data_(data), next_(nullptr) {}
-Node::Node() : data_(-1), next_(nullptr) {}
-Node *Node::next() {return next_;}
-int Node::data() { return data_;}
-void Node::set_next(Node *node) { next_ = node;}
-void Node::set_data(int data) {data_=data;}
