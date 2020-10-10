f9ece87a5c05e07cfa25917aa45c51e045ca331b
seungkyun_park <phcmas1@gmail.com>
2020-10-10 21:59:21 +0900

    commit

--- algorithm/leet_code/2020_10/1010.cc
+++ algorithm/leet_code/2020_10/1010.cc
@@ -39,25 +39,64 @@
  *  -231 <= xstart < xend <= 231 - 1 **/
 
 #include <vector>
+#include <set>
 #include <iostream>
 using namespace std;
 
-int findMinArrowShots(vector<vector<int> >& points) {
+bool notContains (vector<int> &point, int x) {
+    if (point[0] <= x && x <= point[1]) return false;
+    return true;
+}
 
+int minArrowShots (vector<vector<int> >& points, int index) {
+    if (points.empty() || index >= points.size()) return 0;
+    if (index == points.size()-1) return 1;
+
+    int minEnd = points[index][1];
+    int nextIndex = points.size();
+    cout << "minEnd: " << index << endl;
+
+    for (int i=index+1; i< points.size(); ++i) {
+        if (minEnd < points[index][0]) {
+            nextIndex = i;
+            break;
+        }
+
+        minEnd = min (points[index][1], minEnd);
+    }
+
+    cout << "index: " << index << endl;
+    cout << "minEnd: " << index << endl;
+    cout << "nextIndex: " << index << endl;
+    
+    return 1+minArrowShots(points, nextIndex);
+}
+
+int findMinArrowShots(vector<vector<int> >& points) {
+    sort (points.begin(), points.end());
+    return minArrowShots (points, 0);
 }
 
 int main() {
     vector<int> a;
     vector<int> b;
+    vector<int> c;
+    vector<int> d;
     vector<vector<int> > points;
 
-    a.push_back(-1);
-    a.push_back(3);
-    b.push_back(-1);
-    b.push_back(0);
+    a.push_back(10);
+    a.push_back(16);
+    b.push_back(2);
+    b.push_back(8);
+    c.push_back(1);
+    c.push_back(6);
+    d.push_back(7);
+    d.push_back(12);
 
     points.push_back(a);
     points.push_back(b);
+    points.push_back(c);
+    points.push_back(d);
 
     sort(points.begin(), points.end());
 
@@ -65,6 +104,9 @@ int main() {
         cout << points[i][0] << ", " << points[i][1] << endl;
     }
 
+    int answer = findMinArrowShots(points);
+
+    cout << answer << endl;
 
     return 0;
 }
