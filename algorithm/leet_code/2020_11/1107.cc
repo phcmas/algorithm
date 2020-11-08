19de6aaffb919eab2fb69cffac38f7fb695e71a1
seungkyun_park <phcmas1@gmail.com>
2020-11-08 17:00:36 +0900

    commit

--- algorithm/leet_code/2020_11/1107.cc
+++ algorithm/leet_code/2020_11/1107.cc
@@ -38,63 +38,62 @@
 
 using namespace std;
 
-int smallestDivisor(vector<int>& nums, int threshold) {
-    sort(nums.begin(), nums.end());
-    
-    int next, cur, sum;
-    int i = -1;
-    double div;
-    bool found;
-
-    if (nums.empty()) return 0;
-
-    cur = 1;
-    next = nums[0];
-
-    while (true) {
-        found = false;
-
-        for (int j = 1; j <= next/cur; ++j) {
-            div = j * cur;
-            sum = 0;
-            for (int k = 0; k < nums.size(); ++k) {
-                sum += ceil(nums[k]/div);
-            }
-
-            if (sum <= threshold) {
-                found = true;
-                break;
-            }
-        }
-
-        if (found) break;
-
-        i++;
-        cur = nums[i];
-        next = (i == nums.size()-1) ? nums[i] : nums[i+1];
+int getSum(vector<int> &nums, double divisor) {
+    int sum = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+        sum += ceil(nums[i]/divisor);
     }
 
-    return div;
+    return sum;
+}
+
+int smallestDivisor(vector<int>& nums, int threshold) {
+    if (nums.empty()) return 0;
+    sort (nums.begin(), nums.end());
+
+    int left = 1
+    int right = nums.back();
+    int mid, sum;
+    int i = 0;
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+        sum = getSum(nums, mid);
+
+        if (sum <= threshold) right = mid-1;
+        else left = mid+1;
+        i++;
+    }
+
+    cout << "answer: " << left << endl;
+    return left;
 }
 
 int main() {
     int num  = 2;
     double num2 = 3;
-    cout << num/num2 << endl;
-    cout << ceil(num/num2) << endl;
+    //cout << num/num2 << endl;
+    //cout << ceil(num/num2) << endl;
 
     //vector<int> nums {1,2,5,9};
-    //vector<int> nums {2,3,5,7,11};
+    vector<int> nums;
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(5);
+    nums.push_back(7);
+    nums.push_back(11);
     //vector<int> nums {19};
-    vector<int> nums {2620,5743,4716,5581,9508,5404,2448,2224,9682,735,7778,2886,9864,5081,5437,2208,
-    2378,5281,8177,6801,5896,307,9618,7899,2268,986,1114,4600,4093,2476,2623,6312,4061,
-    5846,9245,3952,2344,65,593,6079,8719,8727,717,8740,2940,2291,339,8275,2890,7222};
-    int threshould = 2451;
+    //vector<int> nums {2620,5743,4716,5581,9508,5404,2448,2224,9682,735,7778,2886,9864,5081,5437,2208,
+    //2378,5281,8177,6801,5896,307,9618,7899,2268,986,1114,4600,4093,2476,2623,6312,4061,
+    //5846,9245,3952,2344,65,593,6079,8719,8727,717,8740,2940,2291,339,8275,2890,7222};
+    int threshould = 11;
     int answer = smallestDivisor(nums, threshould);
     // 답은 98
     cout << answer << endl;
 
     return 0;
 }
+
+
 
 
