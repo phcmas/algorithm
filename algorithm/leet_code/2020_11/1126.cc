c8a59272976406d7eba78ebdd01eba2c7e00a957
seungkyun_park <phcmas1@gmail.com>
2020-11-28 21:31:06 +0900

    commit

--- algorithm/leet_code/2020_11/1126.cc
+++ algorithm/leet_code/2020_11/1126.cc
@@ -26,11 +26,62 @@
 
 using namespace std;
 
-int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-            
+bool compare (vector<int> &v1, vector<int> &v2) {
+    if (v1[1] != v2[1]) {
+        return v1[1] < v2[1];
+    }
+
+    return v1[0] < v2[0];
+}
+
+int eraseOverlapIntervals (vector<vector<int>>& intervals) {
+    int prev;
+    int answer;
+
+    sort (intervals.begin(), intervals.end(), compare);
+
+    if (intervals.empty() || intervals.size() == 1) return 0;
+
+    prev = intervals[0][1];
+    answer = 0;
+
+    for (int i = 1; i < intervals.size(); ++i) {
+        if (intervals[i][0] < prev) {
+            answer++;
+        } else {
+            prev = intervals[i][1];
+        }
+    }
+
+    return answer;
 }
 
 int main() {
+    vector<int> i1 {1,2};
+    vector<int> i2 {2,3};
+    vector<int> i3 {3,4};
+    vector<int> i4 {1,3};
+
+    vector<int> j1 {1,2};
+    vector<int> j2 {1,2};
+    vector<int> j3 {1,2};
+
+    vector<int> k1 {1,2};
+    vector<int> k2 {2,3};
+
+    vector<vector<int>> intervals1 {i1,i2,i3,i4};
+    vector<vector<int>> intervals2 {j1,j2,j3};
+    vector<vector<int>> intervals3 {k1,k2};
+
+    int answer1 = eraseOverlapIntervals(intervals1); // 1
+    int answer2 = eraseOverlapIntervals(intervals2); // 2
+    int answer3 = eraseOverlapIntervals(intervals3); // 2
+
+    cout << answer1 << endl;
+    cout << answer2 << endl;
+    cout << answer3 << endl;
 
     return 0;
 }
+
+
