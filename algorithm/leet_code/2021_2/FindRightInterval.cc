035e4f6833383660b15ca0826928c8323eddcc23
seungkyun_park <phcmas1@gmail.com>
2021-02-19 00:21:31 +0900

    commit

--- algorithm/leet_code/2021_2/FindRightInterval.cc
+++ algorithm/leet_code/2021_2/FindRightInterval.cc
@@ -29,29 +29,99 @@
 
 #include <vector>
 #include <iostream>
+#include <stack>
 #include <algorithm>
 
 using namespace std;
 
-bool comp(vector<int> interval1, vector<int> interval2) {
-    return interval1[1] < interval2[1];
+class Triple {
+    public:
+    int num;
+    bool isRight;
+    int index;
+    Triple(int n, bool isR, int i) {
+        num = n;
+        isRight = isR;
+        index = i;
+    }
+};
+
+bool comp(Triple *t1, Triple *t2) {
+    if (t1->num != t2->num) {
+        return t1->num < t2->num;
+    } else if (t1->isRight != t2->isRight) {
+        return t1->isRight == true && t2->isRight == false;
+    }
+
+    return true;
 }
 
 vector<int> findRightInterval(vector<vector<int>> &intervals) {
-    return vector<int>();
+    vector<int> answer (intervals.size(), -1);
+    vector<Triple *> triples;
+    stack<int> st;
+
+    if (intervals.empty()) return vector<int>();
+
+    for (int i = 0; i < intervals.size(); ++i) {
+        for (int j = 0; j < 2; ++j) {
+            Triple triple (intervals[i][j], true, i);
+            triples.push_back(new Triple(intervals[i][j], j, i));
+        }
+    }
+
+    sort(triples.begin(), triples.end(), comp);
+
+    for (int i = 0; i < triples.size(); ++i) {
+        if (triples[i]->isRight) {
+            st.push(triples[i]->index);
+            continue;
+        }
+        
+        while (!st.empty()) {
+            answer[st.top()] = triples[i]->index;
+            st.pop();
+        }
+    }
+
+    for (int i = 0 ; i < triples.size(); ++i) {
+        delete triples[i];
+    }
+
+    return answer;
 }
 
 int main() {
+    vector<int> interval00 {1,2};
+    vector<vector<int>> interval0 {interval00};
+    vector<int> ans0 = findRightInterval(interval0);
+    
     vector<int> interval10 {3, 4};
     vector<int> interval11 {2, 3};
     vector<int> interval12 {1, 2};
     vector<vector<int>> interval1 {interval10, interval11, interval12};
     vector<int> ans1 = findRightInterval(interval1);
 
+    vector<int> interval20 {1,4};
+    vector<int> interval21 {2,3};
+    vector<int> interval22 {3,4};
+    vector<vector<int>> interval2 {interval20, interval21, interval22};
+    vector<int> ans2 = findRightInterval(interval2);
+
+    for (int i = 0; i < ans0.size(); ++i) {
+        cout << ans0[i] << " ";
+    }
+    cout << endl; // -1
+
     for (int i = 0; i < ans1.size(); ++i) {
         cout << ans1[i] << " ";
     }
-    cout << endl;
+    cout << endl; // -1 0 1
+
+    for (int i = 0; i < ans2.size(); ++i) {
+        cout << ans2[i] << " ";
+    }
+    cout << endl; // -1 2 -1
 
     return 0;
 }
