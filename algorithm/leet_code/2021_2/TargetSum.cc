f2f3ff8b2f07d0aa5b0ce7a57cc39022dc05826e
seungkyun_park <phcmas1@gmail.com>
2021-02-21 00:43:56 +0900

    commit

--- algorithm/leet_code/2021_2/TargetSum.cc
+++ algorithm/leet_code/2021_2/TargetSum.cc
@@ -20,21 +20,55 @@
  *  The sum of elements in the given array will not exceed 1000.
  *  Your output answer is guaranteed to be fitted in a 32-bit integer. **/
 
+// dfs는 내가 처음 생각한 풀이. 
+
 #include <vector>
+#include <cstring>
 #include <iostream>
 
 using namespace std;
 
-int backTracking(vector<int> &nums, int index, int sum, int S, int &answer) {
-    if (sum == S) answer++;
+int dp[1000];
+
+int subsetSum(vector<int> &nums, int s) {
+    for (int num : nums) {
+        for (int i = s; i >= num; --i) {
+            dp[i] += dp[i-num];
+        }
+    }
+
+    return dp[s];
+}
+
+void dfs(vector<int> &nums, int index, int sum, int S, int &answer) {
+    if (index == nums.size()) {
+        if (sum == S) answer++;
+        return;
+    }
+
+    dfs(nums, index+1, sum-nums[index], S, answer);
+    dfs(nums, index+1, sum+nums[index], S, answer);
 }
 
 int findTargetSumWays(vector<int> &nums, int S) {
-    int answer = 0;
-    backTracking(nums, 0, 0, S, answer);
-    return answer;
+    int sum = 0;
+    
+    memset(dp, 0 , sizeof(dp));
+    dp[0] = 1;
+    for (int num : nums) sum += num;
+
+    return sum < S || (sum + S) % 2 == 1 ? 0 : subsetSum(nums, (sum+S) / 2);
+
+    //dfs(nums, 0, 0, S, answer);
+    //return answer;
 }
 
 int main() {
+    vector<int> nums0 {1,1,1,1,1};
+    int ans0 = findTargetSumWays(nums0, 3);
+
+    cout << ans0 << endl; // 5
+
     return 0;
 }
+
