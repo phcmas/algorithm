75035bdf82b7433f9c4cb015b592d9253df2a03c
seungkyun_park <phcmas1@gmail.com>
2021-02-21 20:52:59 +0900

    Git Repository 정리 중 필요없던 폴더 제거, 폴더 이동 및 폴더 이름 변경

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/leet_code/2020_12/1209.o"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
