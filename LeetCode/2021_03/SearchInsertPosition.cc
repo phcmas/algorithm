78f011be86fb052bae20c63691a9b1a190353003
seungkyun_park <phcmas1@gmail.com>
2021-03-17 00:24:56 +0900

    commit

--- LeetCode/2021_03/SearchInsertPosition.cc
+++ LeetCode/2021_03/SearchInsertPosition.cc
@@ -35,7 +35,7 @@ using namespace std;
 
 int searchInsert(vector<int>& nums, int target) {
     int left = 0;
-    int right = nums.size();
+    int right = nums.size()-1;
     int middle = 0;
 
     while (left <= right) {
