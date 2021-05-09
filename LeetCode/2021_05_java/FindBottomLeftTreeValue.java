9f1a0a5e3e487a55436915e7dc904a01ec2ab428
seungkyun_park <phcmas1@gmail.com>
2021-05-09 21:48:51 +0900

    commit

---
+++ LeetCode/2021_05_java/FindBottomLeftTreeValue.java
@@ -0,0 +1,54 @@
+/** 513. Find Bottom Left Tree Value */
+/** Given the root of a binary tree, return the leftmost value in the last row of the tree.
+ * 
+ *  Example 1:
+ *  Input: root = [2,1,3]
+ *  Output: 1
+ * 
+ *  Example 2:
+ *  Input: root = [1,2,3,4,null,5,6,null,null,7]
+ *  Output: 7
+ * 
+ *  Constraints:
+ *  The number of nodes in the tree is in the range [1, 10^4].
+ *  -2^31 <= Node.val <= 2^31 - 1 */
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class FindBottomLeftTreeValue {
+    public int findBottomLeftValue(TreeNode root) {
+        TreeNode leftMost = root;
+        Queue<TreeNode> queue = new ArrayDeque<>();
+
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            leftMost = queue.peek();
+
+            for (int i = 0; i < size; ++i) {
+                TreeNode cur = queue.poll();
+
+                if (cur.left != null) queue.add(cur.left);
+                if (cur.right != null) queue.add(cur.right);
+            }
+            
+        }
+
+        return leftMost.val;
+    }
+}
