8e9977d329b39d8ebdad2f525345494de89a802c
seungkyun_park <phcmas1@gmail.com>
2021-05-17 01:54:56 +0900

    LeetCode 언어에 따라 폴더구조 대폭 개편함 C++은 LinkedList TreeNode 등의 common library를 빌드하여 다른 cc 파일에서 헤더를 참조할 수 있도록 CMAKE 만드는 중

---
+++ LeetCode/C++/common/ListNode.cc
@@ -0,0 +1,9 @@
+#include "ListNode.h"
+#include <iostream>
+
+using namespace std;
+
+ListNode::ListNode() : val(0), next(nullptr) {}
+ListNode::ListNode(int x) : val(x), next(nullptr) {}
+ListNode::ListNode(int x, ListNode *next) : val(x), next(next) {}
+void ListNode::printList() { cout << "dfdsdfasf" << endl; } 
---
+++ LeetCode/C++/common/ListNode.h
@@ -0,0 +1,9 @@
+class ListNode {
+public:
+	int val;
+    ListNode *next;
+    ListNode();
+    ListNode(int x);
+	ListNode(int x, ListNode *next);
+	void printList();
+};
