5e71910a94b6ffb9efd4c8dbe8ca6e09ba9fc8d1
seungkyun <phcmas1@gmail.com>
2022-05-12 23:42:29 +0900

    jest test 까지 구성 완료

--- leetcode/typescript/test/2022_05.spec.ts
+++ leetcode/typescript/test/2022_05.spec.ts
@@ -2,6 +2,16 @@ import minimumDeleteSum from "../2022_05/MinimumASCIIDeleteSumForTwoStrings";
 
 describe("2022_05 test", () => {
   it("Minimum ASCII Delete Sum for Two Strings", async () => {
-    minimumDeleteSum("1", "2");
+    const s01 = "sea";
+    const s02 = "eat";
+
+    const s11 = "delete";
+    const s12 = "leet";
+
+    const ans0 = minimumDeleteSum(s01, s02);
+    const ans1 = minimumDeleteSum(s11, s12);
+
+    expect(ans0).toEqual(231);
+    expect(ans1).toEqual(403);
   });
 });
