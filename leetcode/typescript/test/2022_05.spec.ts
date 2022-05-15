015e2fee815af1e36a87cbd01fab438d3b1374d0
seungkyun <phcmas1@gmail.com>
2022-05-15 17:22:44 +0900

    contiguous array

--- leetcode/typescript/test/2022_05.spec.ts
+++ leetcode/typescript/test/2022_05.spec.ts
@@ -1,8 +1,9 @@
 import minimumDeleteSum1 from "../2022_05/MinimumASCIIDeleteSumForTwoStrings-1";
 import minimumDeleteSum2 from "../2022_05/MinimumASCIIDeleteSumForTwoStrings-2";
+import findMaxLength from "../2022_05/ContiguousArray";
 
 describe("2022_05 test", () => {
-  it("Minimum ASCII Delete Sum for Two Strings - 1", async () => {
+  it("Minimum ASCII Delete Sum for Two Strings - 1", () => {
     const s01 = "sea";
     const s02 = "eat";
 
@@ -21,7 +22,7 @@ describe("2022_05 test", () => {
     expect(ans2).toEqual(403);
   });
 
-  it("Minimum ASCII Delete Sum for Two Strings - 2", async () => {
+  it("Minimum ASCII Delete Sum for Two Strings - 2", () => {
     const s01 = "sea";
     const s02 = "eat";
 
@@ -38,5 +39,28 @@ describe("2022_05 test", () => {
     expect(ans0).toEqual(231);
     expect(ans1).toEqual(310);
     expect(ans2).toEqual(403);
+  });
+
+  it("contiguous array", () => {
+    const nums0 = [0, 1];
+    const nums1 = [0, 1, 0];
+    const nums2 = [0, 0, 0, 1, 1, 1, 0];
+    const nums3 = [0, 0, 0, 1, 1, 1, 1];
+    const nums4 = [0, 0, 1, 0, 0, 0, 1, 1];
+    const nums5 = [0, 0, 1];
+
+    const ans0 = findMaxLength(nums0);
+    const ans1 = findMaxLength(nums1);
+    const ans2 = findMaxLength(nums2);
+    const ans3 = findMaxLength(nums3);
+    const ans4 = findMaxLength(nums4);
+    const ans5 = findMaxLength(nums5);
+
+    expect(ans0).toEqual(2);
+    expect(ans1).toEqual(2);
+    expect(ans2).toEqual(6);
+    expect(ans3).toEqual(6);
+    expect(ans4).toEqual(6);
+    expect(ans5).toEqual(2);
   });
 });
