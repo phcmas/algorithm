ef3e246deec3adbd4c96f53d3622b1b399703b29
seungkyun <phcmas1@gmail.com>
2022-06-11 15:35:14 +0900

    commit

--- leetcode/typescript/common/ListNode.ts
+++ leetcode/typescript/common/ListNode.ts
@@ -61,37 +61,58 @@ export class ListNode {
     return node2 === null;
   }
 
-  // circular list는 각 노드의 값을 -10^5 ~ 10^5 으로 제한시킨다
+  static detectCycle(head: ListNode | null) {
+    let slow = head;
+    let fast = head;
+    let index = 0;
+
+    while (slow !== null && fast !== null && fast.next !== null) {
+      slow = slow.next;
+      fast = fast.next.next;
+
+      if (fast === slow) {
+        slow = head;
+
+        while (slow !== null && fast !== null) {
+          if (slow === fast) return { cycle: slow, index };
+
+          slow = slow.next;
+          fast = fast.next;
+          index++;
+        }
+      }
+    }
+
+    return { cycle: null, index: -1 };
+  }
+
   static isSameCircularList(head1: ListNode | null, head2: ListNode | null) {
-    const maxValue = Math.pow(10, 5);
-    const visited = maxValue * 2 + 1;
+    const floyed1 = this.detectCycle(head1);
+    const floyed2 = this.detectCycle(head2);
 
-    let count = 0;
-    let node1 = head1;
-    let node2 = head2;
+    if (floyed1.index !== floyed2.index) return false;
 
-    while (node1 !== null && node1.val <= maxValue) {
-      if (node2 === null) return false;
+    let node1: ListNode | null = head1;
+    let node2: ListNode | null = head2;
+
+    for (let i = 0; i < floyed1.index; ++i) {
+      if (node1 === null) return node2 === null;
+      if (node2 === null) return node1 === null;
       if (node1.val !== node2.val) return false;
 
-      node1.val += visited;
-      node2.val += visited;
       node1 = node1.next;
       node2 = node2.next;
-      count++;
     }
 
-    if (node1 === null) {
-      return node2 === null;
-    } else if (node2 === null || node2.val !== node1.val) {
-      return false;
-    }
-
-    for (let i = 0; i < count; ++i) {
+    while (true) {
+      if (node1 === null) return node2 === null;
+      if (node2 === null) return node1 === null;
       if (node1.val !== node2.val) return false;
+      if (floyed1.cycle === node1) return floyed2.cycle === node2;
+      if (floyed2.cycle === node2) return floyed1.cycle === node1;
+      node1 = node1.next;
+      node2 = node2.next;
     }
-
-    return true;
   }
 
   static splitList(node: ListNode | null, start: number, count: number) {
