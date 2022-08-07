aacc17528bbef2e6e3c498236abb54088fb9b2a4
seungkyun_park <phcmas1@gmail.com>
2022-08-07 17:52:49 +0900

    - deepest leaves sum - longest common subsequence

--- leetcode/typescript/test/2022_08.spec.ts
+++ leetcode/typescript/test/2022_08.spec.ts
@@ -1,6 +1,8 @@
-import { Z_ASCII } from "zlib";
+import { TreeNode } from "../common/TreeNode";
 import { minPairSum } from "../2022_08/MinimizeMaximumPairSumInArray";
 import { pushDominoes } from "../2022_08/PushDominoes";
+import { deepestLeavesSum } from "../2022_08/DeepestLeavesSum";
+import { longestCommonSubsequence } from "../2022_08/LongestCommonSubsequence";
 
 describe("2022_08 test", () => {
   it("push dominoes", () => {
@@ -29,5 +31,57 @@ describe("2022_08 test", () => {
 
     expect(ret0).toEqual(ans0);
     expect(ret1).toEqual(ans1);
+  });
+
+  it("deepest leaves sum", () => {
+    const arr0 = [1, 2, 3, 4, 5, null, 6, 7, null, null, null, null, 8];
+    const arr1 = [6, 7, 8, 2, 7, 1, 3, 9, null, 1, 4, null, null, null, 5];
+
+    const root0 = TreeNode.makeTreeNode(arr0);
+    const root1 = TreeNode.makeTreeNode(arr1);
+
+    const ans0 = 15;
+    const ans1 = 19;
+
+    const ret0 = deepestLeavesSum(root0);
+    const ret1 = deepestLeavesSum(root1);
+
+    expect(ret0).toEqual(ans0);
+    expect(ret1).toEqual(ans1);
+  });
+
+  it("longest common subsequence", () => {
+    const text01 = "abcde";
+    const text02 = "ace";
+
+    const text11 = "abc";
+    const text12 = "abc";
+
+    const text21 = "abc";
+    const text22 = "def";
+
+    const text31 = "ezupkr";
+    const text32 = "ubmrapg";
+
+    const text41 = "pmjghexybyrgzczy";
+    const text42 = "hafcdqbgncrcbihkd";
+
+    const ans0 = 3;
+    const ans1 = 3;
+    const ans2 = 0;
+    const ans3 = 2;
+    const ans4 = 4;
+
+    const ret0 = longestCommonSubsequence(text01, text02);
+    const ret1 = longestCommonSubsequence(text11, text12);
+    const ret2 = longestCommonSubsequence(text21, text22);
+    const ret3 = longestCommonSubsequence(text31, text32);
+    const ret4 = longestCommonSubsequence(text41, text42);
+
+    expect(ret0).toEqual(ans0);
+    expect(ret1).toEqual(ans1);
+    expect(ret2).toEqual(ans2);
+    expect(ret3).toEqual(ans3);
+    expect(ret4).toEqual(ans4);
   });
 });
