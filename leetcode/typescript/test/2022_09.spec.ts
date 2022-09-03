80cd2454500c1a43f621ca21232ffb0e7468fcac
seungkyun <phcmas1@gmail.com>
2022-09-04 01:29:48 +0900

    possible bipartition

--- leetcode/typescript/test/2022_09.spec.ts
+++ leetcode/typescript/test/2022_09.spec.ts
@@ -1,4 +1,5 @@
 import { closedIsland } from "../2022_09/NumberOfClosedIslands";
+import { possibleBipartition } from "../2022_09/PossibleBipartition";
 
 describe("2022_08 test", () => {
   it("push dominoes", () => {
@@ -53,5 +54,41 @@ describe("2022_08 test", () => {
     expect(ret1).toEqual(ans1);
     expect(ret2).toEqual(ans2);
     expect(ret3).toEqual(ans3);
+  });
+
+  it("possible bipartition", () => {
+    const n0 = 4;
+    const n1 = 3;
+    const n2 = 5;
+
+    const dislikes0 = [
+      [1, 2],
+      [1, 3],
+      [2, 4],
+    ];
+    const dislikes1 = [
+      [1, 2],
+      [1, 3],
+      [2, 3],
+    ];
+    const dislikes2 = [
+      [1, 2],
+      [2, 3],
+      [3, 4],
+      [4, 5],
+      [1, 5],
+    ];
+
+    const ans0 = true;
+    const ans1 = false;
+    const ans2 = false;
+
+    const ret0 = possibleBipartition(n0, dislikes0);
+    const ret1 = possibleBipartition(n1, dislikes1);
+    const ret2 = possibleBipartition(n2, dislikes2);
+
+    expect(ret0).toEqual(ans0);
+    expect(ret1).toEqual(ans1);
+    expect(ret2).toEqual(ans2);
   });
 });
