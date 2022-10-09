0c131d9e0f58811929d41cf08668d0fe574e7932
liam-swatchon <liam@swatchon.com>
2022-10-09 18:16:56 +0900

    sum of subarray ranges

--- leetcode/typescript/test/2022_10.spec.ts
+++ leetcode/typescript/test/2022_10.spec.ts
@@ -1,5 +1,7 @@
 import { TreeNode } from "../common/TreeNode";
 import { generateTrees } from "../2022_10/UniqueBinarySearchTreesII";
+import { subArrayRangesI } from "../2022_10/SumOfSubarrayRangesI";
+import { subArrayRangesII } from "../2022_10/SumOfSubarrayRangesII";
 
 describe("2022_10 test", () => {
   it("unique binary search tree II", () => {
@@ -24,5 +26,41 @@ describe("2022_10 test", () => {
 
     expect(compare0).toEqual(true);
     expect(compare1).toEqual(true);
+  });
+
+  it("sum of subarray ranges I", () => {
+    const nums0 = [1, 2, 3];
+    const nums1 = [1, 3, 3];
+    const nums2 = [4, -2, -3, 4, 1];
+
+    const ans0 = 4;
+    const ans1 = 4;
+    const ans2 = 59;
+
+    const ret0 = subArrayRangesI(nums0);
+    const ret1 = subArrayRangesI(nums1);
+    const ret2 = subArrayRangesI(nums2);
+
+    expect(ret0).toEqual(ans0);
+    expect(ret1).toEqual(ans1);
+    expect(ret2).toEqual(ans2);
+  });
+
+  it("sum of subarray ranges II", () => {
+    const nums0 = [1, 2, 3];
+    const nums1 = [1, 3, 3];
+    const nums2 = [4, -2, -3, 4, 1];
+
+    const ans0 = 4;
+    const ans1 = 4;
+    const ans2 = 59;
+
+    const ret0 = subArrayRangesII(nums0);
+    const ret1 = subArrayRangesII(nums1);
+    const ret2 = subArrayRangesII(nums2);
+
+    expect(ret0).toEqual(ans0);
+    expect(ret1).toEqual(ans1);
+    expect(ret2).toEqual(ans2);
   });
 });
