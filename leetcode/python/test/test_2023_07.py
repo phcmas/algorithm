815e71bcfd9e3b8b3e4db107a1799de596c97d41
liam-asleep <liam@asleep.ai>
2023-07-09 22:46:19 +0900

    longest nice substring

--- leetcode/python/test/test_2023_07.py
+++ leetcode/python/test/test_2023_07.py
@@ -8,6 +8,7 @@ sys.path.insert(1, abspath(SRC_DIR))
 # pylint: disable=C0413,R0402
 import year_twenty_three.july.optimal_partition_of_string as partition_string
 import year_twenty_three.july.maximum_sum_of_two_non_overlapping_subarrays as max_sum_two_overlap
+import year_twenty_three.july.longest_nice_substring as longest_nice_substring
 
 
 def test_partition_string():
@@ -37,6 +38,24 @@ def test_max_sum_two_no_overlap():
     result2 = solution2.max_sum_two_no_overlap(nums2, first_len2, second_len2)
 
     answer0, answer1, answer2 = 20, 29, 31
+
+    assert result0 == answer0
+    assert result1 == answer1
+    assert result2 == answer2
+
+
+def test_longest_nice_substring():
+    solution0 = longest_nice_substring.Solution()
+    solution1 = longest_nice_substring.Solution()
+    solution2 = longest_nice_substring.Solution()
+
+    s0, s1, s2 = "YazaAay", "Bb", "c"
+
+    result0 = solution0.longest_nice_substring(s0)
+    result1 = solution1.longest_nice_substring(s1)
+    result2 = solution2.longest_nice_substring(s2)
+
+    answer0, answer1, answer2 = "aAa", "Bb", ""
 
     assert result0 == answer0
     assert result1 == answer1
