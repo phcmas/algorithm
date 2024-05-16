6fa6c8035a68faa748be32f55aaba32ced34a551
seungkyun_park <phcmas1@gmail.com>
2024-05-17 00:55:41 +0900

    lowest common ancestor of deepest leaves

--- leetcode/python/test/test_2024_05.py
+++ leetcode/python/test/test_2024_05.py
@@ -5,6 +5,8 @@ SRC_DIR = join(dirname(__file__), "../src")
 sys.path.insert(1, abspath(SRC_DIR))
 
 # pylint: disable=C0411,C0413,R0402,E0401
+from common.tree_node import TreeNode
+from util import is_same_tree
 import year_twenty_four.may.find_the_distance_value_between_two_arrays as find_the_distance
 import year_twenty_four.may.fair_candy_swap as find_candy_swap
 import year_twenty_four.may.find_the_k_beauty_of_a_number as find_the_k_beauty
@@ -12,6 +14,7 @@ import year_twenty_four.may.binary_trees_with_factors as binary_trees_with_facto
 import year_twenty_four.may.rabbits_in_forest as rabbits_in_forest
 import year_twenty_four.may.longest_happy_string as longest_happy_string
 import year_twenty_four.may.maximum_repeating_substring as maximum_repeating_substring
+import year_twenty_four.may.lowest_common_ancestor_of_deepest_leaves as lca_of_deepest_leaves
 
 
 def test_find_the_distance_value():
@@ -134,3 +137,23 @@ def test_maximum_repeating_substring():
     assert result1 == 1
     assert result2 == 0
     assert result3 == 5
+
+
+def test_deepest_leaves():
+    solution = lca_of_deepest_leaves.Solution()
+
+    root0 = TreeNode.make([3, 5, 1, 6, 2, 0, 8, None, None, 7, 4])
+    root1 = TreeNode.make([1])
+    root2 = TreeNode.make([0, 1, 3, None, 2])
+
+    result0 = solution.lca_deepest_leaves(root0)
+    result1 = solution.lca_deepest_leaves(root1)
+    result2 = solution.lca_deepest_leaves(root2)
+
+    answer0 = TreeNode.make([2, 7, 4])
+    answer1 = TreeNode.make([1])
+    answer2 = TreeNode.make([2])
+
+    assert is_same_tree(result0, answer0)
+    assert is_same_tree(result1, answer1)
+    assert is_same_tree(result2, answer2)
