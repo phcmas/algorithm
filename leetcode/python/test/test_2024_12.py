9392dd7a715e97cc3d047e349b681b6a13e9084a
Liam Park <liam@asleep.ai>
2024-12-01 23:30:58 +0900

    maximum distance in arrays

--- leetcode/python/test/test_2024_12.py
+++ leetcode/python/test/test_2024_12.py
@@ -5,6 +5,7 @@ SRC_DIR = join(dirname(__file__), "../src")
 sys.path.insert(1, abspath(SRC_DIR))
 
 import year_twenty_four.december.diagonal_traverse_ii as diagonal_traverse_ii
+import year_twenty_four.december.maximum_distance_in_arrays as maximum_distance_in_arrays
 
 
 def test_diagonal_traverse_ii():
@@ -18,3 +19,16 @@ def test_diagonal_traverse_ii():
 
     assert result0 == [1, 4, 2, 7, 5, 3, 8, 6, 9]
     assert result1 == [1, 6, 2, 8, 7, 3, 9, 4, 12, 10, 5, 13, 11, 14, 15, 16]
+
+
+def test_maximum_distance_in_arrays():
+    solution = maximum_distance_in_arrays.Solution()
+
+    arrays0 = [[1, 2, 3], [4, 5], [1, 2, 3]]
+    arrays1 = [[1], [1]]
+
+    result0 = solution.max_distance(arrays0)
+    result1 = solution.max_distance(arrays1)
+
+    assert result0 == 4
+    assert result1 == 0
