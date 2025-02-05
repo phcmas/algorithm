61adbed38d10b48f107a37e273960adffe401c3e
Liam Park <liam@asleep.ai>
2025-02-06 01:21:26 +0900

    daily temperatures

--- leetcode/python/test/year_twenty_five/test_2025_02.py
+++ leetcode/python/test/year_twenty_five/test_2025_02.py
@@ -5,6 +5,7 @@ SRC_DIR = join(dirname(__file__), "../../src")
 sys.path.insert(1, abspath(SRC_DIR))
 
 import year_twenty_five.february.meeting_rooms_ii as meeting_rooms_ii
+import year_twenty_five.february.daily_temperatures as daily_temperatures
 
 
 def test_check_if_exist():
@@ -24,3 +25,19 @@ def test_check_if_exist():
     assert result1 == 1
     assert result2 == 3
     assert result3 == 1
+
+
+def test_daily_temperatures():
+    solution = daily_temperatures.Solution()
+
+    temperatures0 = [73, 74, 75, 71, 69, 72, 76, 73]
+    temperatures1 = [30, 40, 50, 60]
+    temperatures2 = [30, 60, 90]
+
+    result0 = solution.daily_temperatures(temperatures0)
+    result1 = solution.daily_temperatures(temperatures1)
+    result2 = solution.daily_temperatures(temperatures2)
+
+    assert result0 == [1, 1, 4, 2, 1, 1, 0, 0]
+    assert result1 == [1, 1, 1, 0]
+    assert result2 == [1, 1, 0]
