d5321e61d237adce56099970c4c9f2037a628472
Liam Park <liam@asleep.ai>
2025-03-02 17:16:10 +0900

    simplify path

--- leetcode/python/test/year_twenty_five/test_2025_03.py
+++ leetcode/python/test/year_twenty_five/test_2025_03.py
@@ -5,6 +5,7 @@ SRC_DIR = join(dirname(__file__), "../../src")
 sys.path.insert(1, abspath(SRC_DIR))
 
 import year_twenty_five.march.evaluate_reverse_polish_notation as eval_rpn
+import year_twenty_five.march.simplify_path as simplify_path
 
 
 def test_eval_rpn():
@@ -21,3 +22,25 @@ def test_eval_rpn():
     assert result0 == 9
     assert result1 == 6
     assert result2 == 22
+
+
+def test_simplify_path():
+    solution = simplify_path.Solution()
+
+    path0 = "/home/"
+    path1 = "/home//foo/"
+    path2 = "/home/user/Documents/../Pictures"
+    path3 = "/../"
+    path4 = "/.../a/../b/c/../d/./"
+
+    result0 = solution.simplify_path(path0)
+    result1 = solution.simplify_path(path1)
+    result2 = solution.simplify_path(path2)
+    result3 = solution.simplify_path(path3)
+    result4 = solution.simplify_path(path4)
+
+    assert result0 == "/home"
+    assert result1 == "/home/foo"
+    assert result2 == "/home/user/Pictures"
+    assert result3 == "/"
+    assert result4 == "/.../b/d"
