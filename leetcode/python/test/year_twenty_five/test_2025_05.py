a525497e663c0fa8443e761d80c3f289169d9d60
Liam Park <liam@asleep.ai>
2025-05-05 21:20:33 +0900

    insert delete get random o1

--- leetcode/python/test/year_twenty_five/test_2025_05.py
+++ leetcode/python/test/year_twenty_five/test_2025_05.py
@@ -5,6 +5,7 @@ SRC_DIR = join(dirname(__file__), "../../src")
 sys.path.insert(1, abspath(SRC_DIR))
 
 import year_twenty_five.may.lru_cache as lru_cache
+import year_twenty_five.may.insert_delete_get_random_o1 as insert_delete_get_random_o1
 
 
 def test_lru_cache():
@@ -62,3 +63,29 @@ def test_lru_cache():
 
     result2 = cache.get(3)
     assert result2 == 2
+
+
+def test_insert_delete_random_o1():
+    solution = insert_delete_get_random_o1.RandomizedSet()
+
+    result0 = solution.insert(0)
+    result1 = solution.insert(1)
+    result2 = solution.insert(1)
+
+    result3 = solution.remove(0)
+    result4 = solution.insert(2)
+    result5 = solution.remove(1)
+    result6 = solution.remove(1)
+
+    result7 = solution.get_random()
+
+    assert result0 is True
+    assert result1 is True
+    assert result2 is False
+
+    assert result3 is True
+    assert result4 is True
+    assert result5 is True
+    assert result6 is False
+
+    assert result7 == 2
