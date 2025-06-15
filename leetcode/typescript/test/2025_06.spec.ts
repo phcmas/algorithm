9d6c0e06a90477cc9a702382e299a46f718cfeb8
Liam Park <liam@asleep.ai>
2025-06-15 16:58:50 +0900

    maximum score from removing substrings

--- leetcode/typescript/test/2025_06.spec.ts
+++ leetcode/typescript/test/2025_06.spec.ts
@@ -1,4 +1,5 @@
 import { minDifference } from "../2025_06/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves";
+import { maximumGain } from "../2025_06/MaximumScoreFromRemovingSubstrings";
 
 describe("2025_06 test", () => {
   it("Minimum Difference Between Largest and Smallest Value in Three Moves", () => {
@@ -6,12 +7,28 @@ describe("2025_06 test", () => {
     const nums1 = [1, 5, 0, 10, 14];
     const nums2 = [3, 100, 20];
 
-    const ans0 = minDifference(nums0);
-    const ans1 = minDifference(nums1);
-    const ans2 = minDifference(nums2);
+    const res0 = minDifference(nums0);
+    const res1 = minDifference(nums1);
+    const res2 = minDifference(nums2);
 
-    expect(ans0).toEqual(0);
-    expect(ans1).toEqual(1);
-    expect(ans2).toEqual(0);
+    expect(res0).toEqual(0);
+    expect(res1).toEqual(1);
+    expect(res2).toEqual(0);
+  });
+
+  it("Maximum Score From Removing Substrings", () => {
+    const s0 = "cdbcbbaaabab";
+    const x0 = 4;
+    const y0 = 5;
+
+    const s1 = "aabbaaxybbaabb";
+    const x1 = 5;
+    const y1 = 4;
+
+    const res0 = maximumGain(s0, x0, y0);
+    const res1 = maximumGain(s1, x1, y1);
+
+    expect(res0).toEqual(19);
+    expect(res1).toEqual(20);
   });
 });
