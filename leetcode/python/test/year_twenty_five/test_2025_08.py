158ef819fecdfc499ec5d96c0c5123b7bf2ed304
Liam Park <liam@asleep.ai>
2025-08-23 23:48:06 +0900

    Greatest Sum Divisible by Three

--- leetcode/python/test/year_twenty_five/test_2025_08.py
+++ leetcode/python/test/year_twenty_five/test_2025_08.py
@@ -4,8 +4,8 @@ from os.path import abspath, dirname, join
 SRC_DIR = join(dirname(__file__), "../../src")
 sys.path.insert(1, abspath(SRC_DIR))
 
-
 import year_twenty_five.august.before_and_after_puzzle as before_and_after_puzzle
+import year_twenty_five.august.greatest_sum_divisible_by_three as max_sum_div_three
 
 
 def test_before_and_after_puzzles():
@@ -38,3 +38,19 @@ def test_before_and_after_puzzles():
         "chocolate bar of soap",
     ]
     assert ret2 == ["a"]
+
+
+def test_max_sum_div_three():
+    solution = max_sum_div_three.Solution()
+
+    nums0 = [3, 6, 5, 1, 8]
+    nums1 = [4]
+    nums2 = [1, 2, 3, 4, 4]
+
+    ret0 = solution.maxSumDivThree(nums0)
+    ret1 = solution.maxSumDivThree(nums1)
+    ret2 = solution.maxSumDivThree(nums2)
+
+    assert ret0 == 18
+    assert ret1 == 0
+    assert ret2 == 12
